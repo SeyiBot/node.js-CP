91b345a4410c817109bb46a2c70e33641c702478
SeyiBot <pricetwice007@gmail.com>
2024-02-27 15:33:14 +0100

    node.js checkpoint

---
+++ emailSender.js
@@ -0,0 +1,28 @@
+
+const nodemailer = require('nodemailer');
+
+// Create a transporter
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'pricetwice007@gmail.com',
+        password: '',
+    },
+    });
+
+    // Define the email options
+    const mailOptions = {
+    from: 'pricetwice007@gmail.com',
+    to: 'willis.e@hotmail.com',
+    subject: 'Test Email',
+    text: 'This is a test email from Node.js using nodemailer.',
+    };
+
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        console.error('Error sending email:', error);
+    } else {
+        console.log('Email sent:', info.response);
+    }
+});
